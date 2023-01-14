63c75e4b29330f792d6bd70a4525fb49af8411f4
Aosen <aosenxiong@126.com>
2023-01-14 16:42:49 -0500

    personal ecommerce project

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa7v8gxnqO0rki035d7',
+        issuer: 'https://dev-16228655.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
